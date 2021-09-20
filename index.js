610c94486d5a0c58c0f8efb853ae6761b2f8c7c7
ibanlopez <lopez.iban@gmail.com>
2021-09-20 16:04:40 +0200

    feat: install amp optimizer

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const AmpOptimizerMiddleware = require('@ampproject/toolbox-optimizer-express');
 
-app.use(express.static('./public'));
+app.use(AmpOptimizerMiddleware.create());
+
+const staticMiddleware = express.static(path.join(__dirname, '/public'));
+app.use(staticMiddleware);
 
 app.listen(5000, () => {
   console.log('Servidor está escuchando en puerto 5000...');
