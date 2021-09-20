65c273398da5a290e8513fb391e832802771a915
ibanlopez <lopez.iban@gmail.com>
2021-09-20 22:23:01 +0200

    feat: make app ready to production on Heroku

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const AmpOptimizerMiddleware = require('@ampproject/toolbox-optimizer-express');
 const path = require('path');
 
 const app = express();
-const AmpOptimizerMiddleware = require('@ampproject/toolbox-optimizer-express');
 
 app.use(AmpOptimizerMiddleware.create());
 
 const staticMiddleware = express.static(path.join(__dirname, '/public'));
 app.use(staticMiddleware);
 
-app.listen(5000, () => {
-  console.log('Servidor está escuchando en puerto 5000...');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Servidor está escuchando en puerto ${port}...`);
 });
